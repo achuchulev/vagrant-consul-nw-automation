321a6060958b17d9c9d2146f9a36d84a5d505c76
Atanas Chuchulev <atanas.v4@gmail.com>
2020-10-12 14:52:32 +0300

    Update nginx_index.sh

--- scripts/nginx_index.sh
+++ scripts/nginx_index.sh
@@ -5,11 +5,6 @@ set -x
 # Remove default index page of nginx
 [ -f /var/www/html/index.nginx-debian.html ] && {
  rm -fr /var/www/html/index.nginx-debian.html
-}  
+}
 
-if [ $(consul catalog nodes -service=web | wc -l) -eq 0 ]; then
-    sleep 10
-    consul catalog nodes -service=web | grep $(hostname) > /var/www/html/index.nginx-debian.html
-else
-    consul catalog nodes -service=web | grep $(hostname) > /var/www/html/index.nginx-debian.html
-fi
+echo $(hostname) > /var/www/html/index.nginx-debian.html
